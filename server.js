88265bb51e3a9d2a2c6e930e3cbbd758997acc75
AlexeyMachehin <99137228+AlexeyMachehin@users.noreply.github.com>
2022-11-06 05:56:56 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/static", express.static(__dirname + "/assets"));
